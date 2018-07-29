ea9c95d2b30c0a182df32408bdd3bf9dd4dfac62
Agfoghlaim <marieohara.gti@gmail.com>
2018-07-29 14:46:38 +0100

    fixed typo

--- config/handle_db.js
+++ config/handle_db.js
@@ -1,5 +1,5 @@
 //use local hardcoded config if not in production, otherwise use heroku env variables
-if(process.env.NOD_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
 	module.exports = require('./mlab_prod')
 }else{
 	module.exports = require('./mlab_dev');
